00ba6caa1ead305349b27ab33aca22fa6b4ffabd
sJagulan <s3881257@student.rmit.edu.au>
2022-10-12 08:14:52 +1100

    first commit

--- person/src/main/java/com/person/PersonApplication.java
+++ person/src/main/java/com/person/PersonApplication.java
@@ -1,4 +1,4 @@
-package com.person;
+package com.person.PersonApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
